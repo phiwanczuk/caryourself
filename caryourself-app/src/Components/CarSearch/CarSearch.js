090bdfb97840206e991646ada528e7034557b13d
Piotrek <phiwanczuk@gmail.com>
2018-01-16 15:42:56 +0100

    carsearch component added

---
+++ caryourself-app/src/Components/CarSearch/CarSearch.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {
+        InputGroup,
+        FormControl,
+        Button,
+        Modal,
+        FormGroup
+} from 'react-bootstrap'
+
+class CarSearch extends React.Component {
+    render() {
+        return (
+
+            <Button>Siema</Button>
+
+
+
+        );
+
+    }
+}
+export default CarSearch;

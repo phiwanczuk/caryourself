cf73cb780f3fee58b79819cd35dda25b3712ea48
Piotrek <phiwanczuk@gmail.com>
2018-01-24 19:34:03 +0100

    input range connection to mock data-base in progress

--- src/Components/Cars/CarSearchForm.js
+++ src/Components/Cars/CarSearchForm.js
@@ -6,8 +6,38 @@ import {
     Modal,
     FormGroup
 } from 'react-bootstrap'
+import InputRange from 'react-input-range'
+import 'react-input-range/lib/css/index.css'
 
 class CarSearchForm extends React.Component {
+
+    state = {
+
+        valueCapacity: {
+            min: 250,
+            max: 800
+        },
+        valueFuel: {
+            min: 4,
+            max: 15
+        },
+        valueSpeed: {
+            min: 150,
+            max: 240
+        }
+
+    }
+    componentDidMount() {
+        fetch(
+            `${process.env.PUBLIC_URL}/MOCK_DATA.json`
+        ).then(
+            response => response.json()
+        ).then(
+            cars => this.setState({cars})
+        );
+        var {id, valueCapacity, valueFuel, valueSpeed} = cars;
+    }
+
 
     handleChange = event => {
         this.setState({
@@ -25,7 +55,14 @@ class CarSearchForm extends React.Component {
                     onChange={this.props.handleChange}
                     type="text"
                     />
+
                 </InputGroup>
+                <InputRange
+                    minValue={250}
+                    maxValue={800}
+                    value={this.state.valueCapacity}
+                    onChange={valueCapacity => this.setState({valueCapacity})}
+                />
             </form>
 
         );
